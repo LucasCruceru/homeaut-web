cc317a055b72df46f3ee1d5afc83bd54b397f31a
lucascruceru <lucascruceru@gmail.com>
2017-07-25 10:03:41 +0300

    made a method that disables buttons depending on the state of the door

--- src/app/dashboard/door-control.component.ts
+++ src/app/dashboard/door-control.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Http } from '@angular/http';
 import {Door} from './dashboard.component';
-import {AppUrl} from '../app.component';
+
 
 @Component ({
   selector: 'app-door-control',
@@ -9,7 +9,7 @@ import {AppUrl} from '../app.component';
   styleUrls: ['./door-control.component.css']
 })
 export class DoorControlComponent {
-
+  appURL = 'http://192.168.216.233:8080';
 
   @Input() door: Door;
 
@@ -21,43 +21,62 @@ export class DoorControlComponent {
   }
 
   openDoor() {
-    this.http.get(AppUrl.appURL + 'dashboard/open/' + this.door.name).subscribe();
+    this.http.get(this.appURL + '/dashboard/open/' + this.door.name).subscribe()
   }
 
   closeDoor() {
-    this.http.get(AppUrl.appURL + 'dashboard/close/' + this.door.name).subscribe();
+    this.http.get(this.appURL + '/dashboard/close/' + this.door.name).subscribe()
   }
 
   stopDoor() {
-    this.http.get(AppUrl.appURL + 'dashboard/stop/' + this.door.name).subscribe();
+    this.http.get(this.appURL + '/dashboard/stop/' + this.door.name).subscribe()
   }
 
   getStatusPercent() {
-    this.http.get(AppUrl.appURL + 'api/doors/1/status/').subscribe(data => {
-      this.statusPercent = Number(data['_body']);
-    });
+    this.http.get(this.appURL + '/dashboard/status/' + this.door.name).subscribe(data => {
+      this.statusPercent = Number(data["_body"]);
+    })
 
     this.getStatus();
     this.showProgress();
+    this.disable();
   }
 
   interval = setInterval(() => {
-    this.getStatusPercent();
+    this.getStatusPercent()
   }, 200);
 
 
   getStatus() {
-    if (this.statusPercent < 10) {
-      return this.statusDoor = 'Closed';
-    } else if (this.statusPercent > 10 && this.statusPercent < 90) { // #1e2127
-      return this.statusDoor = 'Moving';
-    } else if (this.statusPercent > 90) {
-      return this.statusDoor = 'Open';
+    if (this.statusPercent < 5) {
+      return this.statusDoor = "Closed";
+    } else if (this.statusPercent > 5 && this.statusPercent < 95) {
+      return this.statusDoor = "Moving";
+    } else if (this.statusPercent > 95) {
+      return this.statusDoor = "Open";
     }
   }
 
-  showProgress() {
-    document.getElementsByClassName('list-group-progress')[0].setAttribute('style', 'width: 0; width: ' + this.statusPercent + '%');
+  showProgress(){
+    document.getElementsByClassName("list-group-progress")[0].setAttribute("style", "width: 0; width: " + this.statusPercent + "%");
   }
 
+  disable(){
+    if (this.statusPercent < 5) {
+      document.getElementById("1").removeAttribute("disabled");
+      document.getElementById("2").setAttribute("disabled", "");
+      document.getElementById("3").setAttribute("disabled", "");
+      // return this.statusDoor = "Closed";
+    } else if (this.statusPercent > 5 && this.statusPercent < 95) {
+      document.getElementById("1").removeAttribute("disabled");
+      document.getElementById("2").removeAttribute("disabled");
+      document.getElementById("3").removeAttribute("disabled");
+      //return this.statusDoor = "Moving";
+    } else if (this.statusPercent > 95) {
+      document.getElementById("1").removeAttribute("disabled");
+      document.getElementById("2").setAttribute("disabled", "");
+      document.getElementById("3").removeAttribute("disabled");
+      //return this.statusDoor = "Open";
+    }
+  }
 }
