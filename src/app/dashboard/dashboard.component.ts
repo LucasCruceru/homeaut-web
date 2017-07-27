9338a59af47f4d5ee9279b7538d4851ef989b08a
lucascruceru <lucascruceru@gmail.com>
2017-07-27 14:24:55 +0300

    made the dashboard to only read 2 doors <- this is a fix for a weird overriding bug we could not figure out

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,17 +8,9 @@ export class Door {
 
   id: number;
   name: string;
+  deviceComm : string;
 
 }
-
-const DOORS: Door[] = [
-  {id: 1, name: 'Front Door'},
-  {id: 2, name: 'Back Door'},
-  {id: 3, name: 'Garage Door'},
-  {id: 4, name: 'Lucas'},
-
-];
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -26,13 +18,17 @@ const DOORS: Door[] = [
 })
 export class DashboardComponent implements OnInit {
 
-
-
-  // doors = DOORS;
+  appURL = 'http://192.168.216.233:8080';
 
   doors: any = [];
 
+  index : number;
 
+  statusPercent1: number;
+  statusPercent2: number;
+
+  statusDoor1: string;
+  statusDoor2: string;
 
   ngOnInit(): void {
     this.getDoorData();
@@ -42,11 +38,115 @@ export class DashboardComponent implements OnInit {
     this.titleService.setTitle('Dashboard');
   }
 
+  openDoor(index: number) {
+    this.http.get(this.appURL + '/dashboard/open/' + this.doors[index].name).subscribe()
+  }
+
+  closeDoor(index: number) {
+    this.http.get(this.appURL + '/dashboard/close/' + this.doors[index].name).subscribe()
+  }
+
+  stopDoor(index: number) {
+    this.http.get(this.appURL + '/dashboard/stop/' + this.doors[index].name).subscribe()
+  }
+
+  getStatusPercentDoor1() {
+    this.http.get(this.appURL + '/dashboard/status/' + this.doors[0].name).subscribe(data => {
+      this.statusPercent1 = data.json()[this.doors[0].deviceComm];
+    })
+
+    this.getStatus1();
+    this.showProgress1();
+    this.disable1();
+  }
+
+  getStatusPercentDoor2() {
+    this.http.get(this.appURL + '/dashboard/status/' + this.doors[1].name).subscribe(data => {
+      this.statusPercent2 = data.json()[this.doors[1].deviceComm];
+    })
+
+    this.getStatus2();
+    this.showProgress2();
+    this.disable2();
+  }
+
+  interval = setInterval(() => {
+    this.getStatusPercentDoor1()
+    this.getStatusPercentDoor2()
+  }, 200);
+
+
+  getStatus1() {
+    if (this.statusPercent1 < 5) {
+      return this.statusDoor1 = "Closed";
+    } else if (this.statusPercent1> 5 && this.statusPercent1 < 95) {
+      return this.statusDoor1 = "Moving";
+    } else if (this.statusPercent1 > 95) {
+      return this.statusDoor1 = "Open";
+    }
+  }
+
+  getStatus2() {
+    if (this.statusPercent2 < 5) {
+      return this.statusDoor2 = "Closed";
+    } else if (this.statusPercent2 > 5 && this.statusPercent2 < 95) {
+      return this.statusDoor2 = "Moving";
+    } else if (this.statusPercent2 > 95) {
+      return this.statusDoor2 = "Open";
+    }
+  }
+
+  showProgress1(){
+    document.getElementById("bar1").setAttribute("style", "width: 0; width: " + this.statusPercent1 + "%");
+  }
+  showProgress2(){
+    document.getElementById("bar2").setAttribute("style", "width: 0; width: " + this.statusPercent2 + "%");
+  }
+
+  disable1(){
+    if (this.statusPercent1 < 5) {
+      document.getElementById("1").removeAttribute("disabled");
+      document.getElementById("2").setAttribute("disabled", "");
+      document.getElementById("3").setAttribute("disabled", "");
+      // return this.statusDoor = "Closed";
+    } else if (this.statusPercent1 > 5 && this.statusPercent1 < 95) {
+      document.getElementById("1").removeAttribute("disabled");
+      document.getElementById("2").removeAttribute("disabled");
+      document.getElementById("3").removeAttribute("disabled");
+      //return this.statusDoor = "Moving";
+    } else if (this.statusPercent1 > 95) {
+      document.getElementById("1").setAttribute("disabled", "");
+      document.getElementById("2").removeAttribute("disabled");
+      document.getElementById("3").setAttribute("disabled", "");
+      //return this.statusDoor = "Open";
+    }
+  }
+
+  disable2(){
+    if (this.statusPercent2 < 5) {
+      document.getElementById("4").removeAttribute("disabled");
+      document.getElementById("5").setAttribute("disabled", "");
+      document.getElementById("6").setAttribute("disabled", "");
+      // return this.statusDoor = "Closed";
+    } else if (this.statusPercent2 > 5 && this.statusPercent2 < 95) {
+      document.getElementById("4").removeAttribute("disabled");
+      document.getElementById("5").removeAttribute("disabled");
+      document.getElementById("6").removeAttribute("disabled");
+      //return this.statusDoor = "Moving";
+    } else if (this.statusPercent2 > 95) {
+      document.getElementById("4").setAttribute("disabled", "");
+      document.getElementById("5").removeAttribute("disabled");
+      document.getElementById("6").setAttribute("disabled", "");
+      //return this.statusDoor = "Open";
+    }
+  }
+
   getDoorData() {
     this.http.get(GlobalVar.appURL + 'dashboard')
       .map(response => response.json())
       .subscribe(data => {
         this.doors = data;
+        console.log(data);
       });
   }
 
--- src/app/dashboard/door-control.component.ts
+++ src/app/dashboard/door-control.component.ts
@@ -2,6 +2,9 @@ import { Component, Input } from '@angular/core';
 import { Http } from '@angular/http';
 import {Door} from './dashboard.component';
 
+export class Status{
+  COMM : number;
+}
 
 @Component ({
   selector: 'app-door-control',
@@ -12,6 +15,8 @@ export class DoorControlComponent {
   appURL = 'http://192.168.216.233:8080';
 
   @Input() door: Door;
+
+  status : Status;
 
   statusPercent: number;
 
@@ -34,7 +39,7 @@ export class DoorControlComponent {
 
   getStatusPercent() {
     this.http.get(this.appURL + '/dashboard/status/' + this.door.name).subscribe(data => {
-      this.statusPercent = Number(data["_body"]);
+      this.statusPercent = data.json()[this.door.deviceComm];
     })
 
     this.getStatus();
